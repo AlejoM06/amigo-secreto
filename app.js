29ea72edf83a0edf2dfefea832103946e6ce430c
Alejo <alejomartine.06@gmail.com>
2025-02-05 15:20:55 -0500

    Creacion la funcion para actualizar lista amigos

--- app.js
+++ app.js
@@ -9,13 +9,29 @@ function agregarAmigo() {
       alert("Por favor, inserte un nombre.");
       return; // Detener la ejecución si el campo está vacío
     }
-  
+
     // Actualizar el array de amigos
     amigos.push(nombre);
-  
+
     // Mostrar los amigos en la lista (actualizar la interfaz)
     actualizarListaAmigos();
-  
+
     // Limpiar el campo de entrada
     input.value = "";
   }
+
+//funcion para actualizar lista amigos
+function actualizarListaAmigos() {
+    const lista = document.getElementById('listaAmigos');
+    //Limpiar lista actual
+    lista.innerHTML = ""; 
+
+//Recorrer el array de amigos y a;adir cada nombre a la lista
+amigos.forEach((amigo) => {
+const li = document.createElement('li');
+li.textContent = amigo;
+lista.appendChild(li);
+})
+
+
+}
